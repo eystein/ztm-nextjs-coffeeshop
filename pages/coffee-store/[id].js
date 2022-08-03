27ce9f530d6e12ef93aa008e348e8234099d9907
eystein <eystein1@gmail.com>
2022-08-03 13:45:24 +0200

    Link component

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,17 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const CoffeeStore = () => {
 	const router = useRouter()
 	console.log('router', router)
-	return <div>Coffee store page {router.query.id}</div>
+	return (
+		<>
+			<Link href="/">
+				<a>Back to home</a>
+			</Link>
+			<div>Coffee store page {router.query.id}</div>
+		</>
+	)
 }
 
 export default CoffeeStore;
