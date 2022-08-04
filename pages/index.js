2cf7bba9c45f9d935b350eaa58c51c92c661df5f
eystein <eystein1@gmail.com>
2022-08-04 11:16:42 +0200

    Grid layout for cards

--- pages/index.js
+++ pages/index.js
@@ -23,11 +23,14 @@ export default function Home() {
 				<div className={styles.heroImage}>
 					<Image src="/static/hero-image.png" alt="Illustration of woman sitting on a cloud and drinking coffee." width={700} height={400} />
 				</div>
-				<Card 
-					name="DarkHourse Coffee"  
-					imgUrl="/static/hero-image.png"
-					href="/coffee-store/darkhorse-coffee"
-					/>
+				<div className={styles.cardLayout}>
+					<Card 
+						name="DarkHourse Coffee"  
+						imgUrl="/static/hero-image.png"
+						href="/coffee-store/darkhorse-coffee"
+						className={styles.card}
+						/>
+				</div>
 			</main>
 
 			
