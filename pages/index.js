5bb46723071ddc43aa060480db5c06157d0f8e6b
eystein <eystein1@gmail.com>
2022-08-18 12:13:07 +0200

    Prettier formatting

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Banner from '../components/banner'
-import Card from '../components/card'
-import styles from '../styles/Home.module.css'
-import { fetchCoffeeStores } from '../lib/coffee-stores'
+import Head from 'next/head';
+import Image from 'next/image';
+import Banner from '../components/banner';
+import Card from '../components/card';
+import styles from '../styles/Home.module.css';
+import { fetchCoffeeStores } from '../lib/coffee-stores';
 
 export async function getStaticProps(context) {
-	
 	const coffeeStores = await fetchCoffeeStores();
 
-	return {		
+	return {
 		props: {
-			coffeeStores: coffeeStores, 
+			coffeeStores: coffeeStores,
 		}, // will be passed to the page component as props
-	}
-	
+	};
 }
-
 
 export default function Home(props) {
 	const handleOnBannerBtnClick = () => {
-		console.log('Button clicked')
-	}
+		console.log('Button clicked');
+	};
 
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Coffee Connoisseur</title>
-				<meta name="description" content="A next.js test app for Eystein" />
-				<link rel="icon" href="/favicon.ico" />
+				<meta name='description' content='A next.js test app for Eystein' />
+				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
 			<main className={styles.main}>
-				<Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick} />
+				<Banner
+					buttonText='View stores nearby'
+					handleOnClick={handleOnBannerBtnClick}
+				/>
 				<div className={styles.heroImage}>
-					<Image src="/static/hero-image.png" alt="Illustration of woman sitting on a cloud and drinking coffee." width={700} height={400} />
+					<Image
+						src='/static/hero-image.png'
+						alt='Illustration of woman sitting on a cloud and drinking coffee.'
+						width={700}
+						height={400}
+					/>
 				</div>
 				{props.coffeeStores.length > 0 && (
 					<>
@@ -43,21 +48,21 @@ export default function Home(props) {
 							{props.coffeeStores.map((coffeeStore) => {
 								return (
 									<Card
-									key={coffeeStore.fsq_id}	
-									name={coffeeStore.name}
-										imgUrl={coffeeStore.imgUrl || "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" }
-										href={`/coffee-store/${coffeeStore.fsq_id}`}
+										key={coffeeStore.id}
+										name={coffeeStore.name}
+										imgUrl={
+											coffeeStore.imgUrl ||
+											'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+										}
+										href={`/coffee-store/${coffeeStore.id}`}
 										className={styles.card}
-										
 									/>
-								)
+								);
 							})}
 						</div>
 					</>
-				)}									
+				)}
 			</main>
-
-			
 		</div>
-	)
+	);
 }
