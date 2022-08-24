045ed30ae03cc390eca2045621c18d8f55ff3485
eystein <eystein1@gmail.com>
2022-08-24 17:07:08 +0200

    Display fetched stores with useState().

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Card from "../components/card";
 import styles from "../styles/Home.module.css";
 import { fetchCoffeeStores } from "../lib/coffee-stores";
 import useTrackLocation from "../hooks/use-track-location";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export async function getStaticProps(context) {
   const coffeeStores = await fetchCoffeeStores();
@@ -23,6 +23,8 @@ export default function Home(props) {
   const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
     useTrackLocation();
 
+  const [coffeeStores, setCoffeeStores] = useState("");
+
   useEffect(() => {
     async function setCoffeeStoresByLocation() {
       if (latLong) {
@@ -30,6 +32,7 @@ export default function Home(props) {
           const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 8);
           console.log({ fetchedCoffeeStores });
           // set coffee stores
+          setCoffeeStores(fetchedCoffeeStores);
         } catch (error) {
           // set error
           console.log("error: ", { error });
@@ -69,6 +72,27 @@ export default function Home(props) {
             height={400}
           />
         </div>
+        {coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores near me</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
         {props.coffeeStores.length > 0 && (
           <div className={styles.sectionWrapper}>
             <h2 className={styles.heading2}>Chamonix stores</h2>
