427ad599f778d4b700b4fd06f9b6bde88faa289a
eystein <eystein1@gmail.com>
2022-09-01 14:25:57 +0200

    Require Airtable

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,7 @@
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_KEY
+);
+
+// Make const with everything from the table
+const table = base("coffee-stores");
