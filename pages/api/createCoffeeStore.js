559d8e5f21a4774aface5020465753b5aa786065
eystein <eystein1@gmail.com>
2022-09-07 13:19:26 +0200

    Refactor! Move airtable connecting, and repeating code into new library module.

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,10 +1,4 @@
-const Airtable = require("airtable");
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_KEY
-);
-
-// Make const with everything from the table
-const table = base("coffee-stores");
+import { table, getMinifiedRecords } from "../../lib/airtable";
 
 const createCoffeeStore = async (req, res) => {
   // check if POST (not GET)
@@ -26,12 +20,7 @@ const createCoffeeStore = async (req, res) => {
         // if it exists
         if (findCoffeeStoreRecords.length !== 0) {
           // loop the array and find the fields object
-          const records = findCoffeeStoreRecords.map((record) => {
-            return {
-              ...record.fields,
-            };
-          });
-
+          const records = getMinifiedRecords(findCoffeeStoreRecords); // function in airtable.js
           // only return the records, not the whole table
           res.json(records);
         } else {
@@ -52,16 +41,12 @@ const createCoffeeStore = async (req, res) => {
             ]);
 
             // Same filtering as if exists above
-            const records = createRecords.map((record) => {
-              return {
-                ...record.fields,
-              };
-            });
+            const records = getMinifiedRecords(createRecords); // function in airtable.js
             // Return only the filtered records fields
             res.json(records);
           } else {
             res.status(400);
-            res.json({ message: "Name is missing." });
+            res.json({ message: "Id or name is missing." });
           }
         }
       } else {
