3440605aa889195aa830b7b46ec8739193c5a0c9
eystein <eystein1@gmail.com>
2022-09-07 10:31:09 +0200

    Catch and return error

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -11,29 +11,39 @@ console.log({ table });
 const createCoffeeStore = async (req, res) => {
   // check if POST (not GET)
   if (req.method === "POST") {
-    // find a record
-    const findCoffeeStoreRecords = await table
-      .select({
-        filterByFormula: `id="0"`,
-      })
-      .firstPage();
+    // use "try" so I can catch errors
+    try {
+      // find a record
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="0"`,
+        })
+        .firstPage();
 
-    console.log({ findCoffeeStoreRecords });
+      console.log({ findCoffeeStoreRecords });
 
-    // if it exists
-    if (findCoffeeStoreRecords.length !== 0) {
-      // loop the array and find the fields object
-      const records = findCoffeeStoreRecords.map((record) => {
-        return {
-          ...record.fields,
-        };
-      });
+      // if it exists
+      if (findCoffeeStoreRecords.length !== 0) {
+        // loop the array and find the fields object
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
 
-      // only return the records, not the whole table
-      res.json(records);
-    } else {
-      // create a record
-      res.json({ message: "create a record" });
+        // only return the records, not the whole table
+        res.json(records);
+      } else {
+        // create a record
+        res.json({ message: "create a record" });
+      }
+    } catch (err) {
+      // console log error
+      console.log("Error finding store", err);
+      // Send a response
+      res.status(500);
+      // Also give user an error
+      res.json({ message: "Error finding store", err });
     }
   }
 };
