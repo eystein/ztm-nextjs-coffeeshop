e425590995a3de63e59105bc3e2c6f667c51618b
eystein <eystein1@gmail.com>
2022-09-07 12:54:28 +0200

    Check if id and name exists, and set up error handling.

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -14,56 +14,67 @@ const createCoffeeStore = async (req, res) => {
 
     // use "try" so I can catch errors
     try {
-      // find a record
-      const findCoffeeStoreRecords = await table
-        .select({
-          filterByFormula: `id=${id}`,
-        })
-        .firstPage();
+      // only run if there is an id
+      if (id) {
+        // find a record
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id=${id}`,
+          })
+          .firstPage();
 
-      // if it exists
-      if (findCoffeeStoreRecords.length !== 0) {
-        // loop the array and find the fields object
-        const records = findCoffeeStoreRecords.map((record) => {
-          return {
-            ...record.fields,
-          };
-        });
+        // if it exists
+        if (findCoffeeStoreRecords.length !== 0) {
+          // loop the array and find the fields object
+          const records = findCoffeeStoreRecords.map((record) => {
+            return {
+              ...record.fields,
+            };
+          });
 
-        // only return the records, not the whole table
-        res.json(records);
+          // only return the records, not the whole table
+          res.json(records);
+        } else {
+          // create a record
+
+          if (id && name) {
+            const createRecords = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  voting,
+                  imgUrl,
+                },
+              },
+            ]);
+
+            // Same filtering as if exists above
+            const records = createRecords.map((record) => {
+              return {
+                ...record.fields,
+              };
+            });
+            // Return only the filtered records fields
+            res.json(records);
+          } else {
+            res.status(400);
+            res.json({ message: "Name is missing." });
+          }
+        }
       } else {
-        // create a record
-
-        const createRecords = await table.create([
-          {
-            fields: {
-              id,
-              name,
-              address,
-              neighbourhood,
-              voting,
-              imgUrl,
-            },
-          },
-        ]);
-
-        // Same filtering as if exists above
-        const records = createRecords.map((record) => {
-          return {
-            ...record.fields,
-          };
-        });
-        // Return only the filtered records fields
-        res.json(records);
+        res.status(400);
+        res.json({ message: "Id is missing." });
       }
     } catch (err) {
       // console log error
-      console.log("Error finding store", err);
+      console.log("Error creating or finding a store", err);
       // Send a response
       res.status(500);
       // Also give user an error
-      res.json({ message: "Error finding store", err });
+      res.json({ message: "Error creating or finding a store", err });
     }
   }
 };
