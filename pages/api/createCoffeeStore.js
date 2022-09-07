7fd5f53b381c1e59321255d4c54f7562e8dbbca4
eystein <eystein1@gmail.com>
2022-09-07 10:03:56 +0200

    Check if record exists in table, and return it.

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -6,12 +6,28 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 // Make const with everything from the table
 const table = base("coffee-stores");
 
-const createCoffeeStore = (req, res) => {
-  // check if the API can POST or GET
+console.log({ table });
+
+const createCoffeeStore = async (req, res) => {
+  // check if POST (not GET)
   if (req.method === "POST") {
-    res.json({ message: "hi there!" });
-  } else {
-    res.json({ message: "method is GET" });
+    // find a record
+    const findCoffeeStoreRecords = await table
+      .select({
+        filterByFormula: `id="0"`,
+      })
+      .firstPage();
+
+    console.log({ findCoffeeStoreRecords });
+
+    // if it exists
+    if (findCoffeeStoreRecords.length !== 0) {
+      // then return the records
+      res.json(findCoffeeStoreRecords);
+    } else {
+      // create a record
+      res.json({ message: "create a record" });
+    }
   }
 };
 
